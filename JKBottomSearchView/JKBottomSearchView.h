f912b39c56d7eb4b416b06c57b323c00502459be
Jarosław Krajewski <jaroslaw.krajewski94@gmail.com>
2018-04-06 19:09:25 +0200

    Initial Commit

---
+++ JKBottomSearchView/JKBottomSearchView.h
@@ -0,0 +1,19 @@
+//
+//  JKBottomSearchView.h
+//  JKBottomSearchView
+//
+//  Created by Jarosław Krajewski on 06/04/2018.
+//  Copyright © 2018 com.jerronimo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JKBottomSearchView.
+FOUNDATION_EXPORT double JKBottomSearchViewVersionNumber;
+
+//! Project version string for JKBottomSearchView.
+FOUNDATION_EXPORT const unsigned char JKBottomSearchViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JKBottomSearchView/PublicHeader.h>
+
+
